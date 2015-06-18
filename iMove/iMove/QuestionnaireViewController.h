efb5fd780239c70319238e22c1f563a6e9b91254
mylesbarros <barrosmyles@gmail.com>
2015-06-18 18:29:15 +0200

    Added view constraints to questionairre, form validation

--- iMove/iMove/QuestionnaireViewController.h
+++ iMove/iMove/QuestionnaireViewController.h
@@ -10,4 +10,16 @@
 
 @interface QuestionnaireViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *age;
+@property (weak, nonatomic) IBOutlet UITextField *weight;
+@property (weak, nonatomic) IBOutlet UISwitch *smokerState;
+@property (weak, nonatomic) IBOutlet UITextField *hdl;
+@property (weak, nonatomic) IBOutlet UITextField *ldl;
+@property (weak, nonatomic) IBOutlet UITextField *bloodPressure;
+
+@property (weak, nonatomic) IBOutlet UILabel *alert;
+@property (weak, nonatomic) IBOutlet UILabel *alertMessage;
+
+- (IBAction)submitRiskAssesment:(id)sender;
+
 @end
